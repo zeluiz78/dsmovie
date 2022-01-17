c75fcb4b27dd470dbf638c5bb12a794cfee299cf
José Luiz <joseluizmartinsf@gmail.com>
2022-01-16 22:50:40 -0300

    useParams

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,12 +4,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,27 +1,13 @@
-import MovieScore from "components/MovieScore";
-import { Link } from "react-router-dom";
-import { Movie } from "types/movie";
+import FormCard from 'components/FormCard';
+import { useParams } from 'react-router-dom';
 
-type Props = {
-    movie: Movie;
-}
+function Form() {
 
-function MovieCard( { movie } : Props) {
+    const params = useParams();
 
     return (
-        <div>
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsmovie-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <MovieScore count={movie.count} score={movie.score} />
-
-                <Link to={`/form/${movie.id}`}>
-                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-                </Link>
-
-            </div>
-        </div>
+        <FormCard movieId={`${params.movieId}`} />
     );
 }
 
-export default MovieCard;
+export default Form;
