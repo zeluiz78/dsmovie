6b9092c18e502e90fe3acabfe075cbf62db21fd4
José Luiz <joseluizmartinsf@gmail.com>
2022-01-16 22:45:06 -0300

    import axios from axios;

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,52 +1,27 @@
+import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
-import "./styles.css";
+import { Movie } from "types/movie";
 
-function Form() {
-  const movie = {
-    id: 1,
-    image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5,
-  };
-
-  return (
-    <div className="dsmovie-form-container">
-      <img
-        className="dsmovie-movie-card-image"
-        src={movie.image}
-        alt={movie.title}
-      />
-      <div className="dsmovie-card-bottom-container">
-        <h3>"The Witcher"</h3>
-        <form className="dsmovie-form">
-          <div className="form-group dsmovie-form-group">
-            <label htmlFor="email">Informe seu email</label>
-            <input type="email" className="form-control" id="email" />
-          </div>
-          <div className="form-group dsmovie-form-group">
-            <label htmlFor="score">Informe sua avaliação</label>
-            <select className="form-control" id="score">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div>
-          <div className="dsmovie-form-btn-container">
-            <button type="submit" className="btn btn-primary dsmovie-btn">
-              Salvar
-            </button>
-          </div>
-        </form>
-        <Link to="/">
-          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
-        </Link>
-      </div>
-    </div>
-  );
+type Props = {
+    movie: Movie;
 }
 
-export default Form;
+function MovieCard( { movie } : Props) {
+
+    return (
+        <div>
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <MovieScore count={movie.count} score={movie.score} />
+
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
+
+            </div>
+        </div>
+    );
+}
+
+export default MovieCard;
